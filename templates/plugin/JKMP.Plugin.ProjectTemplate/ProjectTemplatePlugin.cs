bae6b65e9d4f6a5ef8749af0fd0d067f1961dbbd
Jonathan Lindahl <jonathanjl99@gmail.com>
2022-05-02 01:14:50 +0200

    Added template plugin class

---
+++ templates/plugin/JKMP.Plugin.ProjectTemplate/ProjectTemplatePlugin.cs
@@ -0,0 +1,20 @@
+namespace JKMP.Plugin.ProjectTemplate;
+
+public class ProjectTemplatePlugin : JKMP.Core.Plugins.Plugin
+{
+    public override void OnLoaded()
+    {
+        // Called when this plugin has been loaded
+    }
+
+    public override void CreateInputActions()
+    {
+        // Called when input actions should be created.
+        // Look at the intellisense for Input.RegisterAction for more info.
+    }
+
+    public override void Initialize()
+    {
+        // Called when all plugins have been loaded
+    }
+}
